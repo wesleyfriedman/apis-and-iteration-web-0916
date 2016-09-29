4372446f9e10c9fa369b0ff7de1ccd3d0a4f7494
wesleyfriedman <friedman.wesley@gmail.com>
2016-09-29 18:19:13 -0400

    first method done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,22 +1,37 @@
 require 'rest-client'
+require 'JSON'
+require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  # iterate ove the character hash to find the collection of `films` for the given
+  # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
-  # return value of this method should be collection of info about each film. 
+  film_array = character_hash.fetch("results").map do |char|
+    if char.fetch("name") == character
+      character["films"].map do |movie_url|
+        film = RestClient.get(movie_url)
+        JSON.parse(film)
+      end
+    end
+  end.flatten
+  # binding.pry
+  # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list 
+  #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
 end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  binding.pry
+  films_hash.each do |film|
+    puts film.fetch("title")
+  end
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,9 +1,10 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Star Wars Information: Everything You Have Ever Wanted To Know"
 end
 
 def get_character_from_user
   puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased. 
+  # use gets to capture the user's input. This method should return that input, downcased.
+  character = gets.chomp.downcase
 end
 
